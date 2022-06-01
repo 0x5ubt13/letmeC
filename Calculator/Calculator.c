7610a08e81429e649d29dda22a6384928fc16a3a
0x5ubt13 <5ubt13@protonmail.com>
2022-06-01 21:19:17 +0100

    upgraded version

--- Calculator/Calculator.c
+++ Calculator/Calculator.c
@@ -1,48 +1,58 @@
 #include <stdio.h>
 
-int print_menu(void);
+char choice(void);
+void calculate(char choice);
 
 int main(void)
 {
-    print_menu();
-    int choice;
-    long result;
-    scanf("%d", &choice);
+    float x, y, result;
+    char operator = choice();
+    calculate(operator);
 
-	long x;
-	long y;
-	
-	printf("Enter x: ");
-	scanf("%ld", &x);
-
-	printf("Enter y: ");
-	scanf("%ld", &y);
-
-    switch (choice)
-    {
-        case 1:
-            result = x + y;
-            break;
-        case 2:
-            result = x - y;
-            break;
-        case 3:
-            result = x * y;
-            break;
-        case 4:
-            result = x / y;
-            break;
-    }
-
-	printf("The result is: %li\n", result);
 	return 0;
 }
 
-int print_menu(void)
+char choice(void)
 {
-    printf("Select 1 for +\n");
-    printf("Select 2 for -\n");
-    printf("Select 3 for *\n");
-    printf("Select 4 for /\n");
-    return 0;
+    printf("Enter + for addition\n");
+    printf("Enter - for subtraction\n");
+    printf("Enter * for multiplication\n");
+    printf("Enter / for division\n");
+
+    char input;
+    scanf("%c", &input);
+    
+    return input;
+}
+
+void calculate(char choice)
+{
+    float result;
+    
+    float x, y;
+    printf("Enter x: ");
+    scanf("%f", &x); 
+	
+    printf("Enter y: ");
+    scanf("%f", &y);
+
+    switch(choice)
+    {
+        case '+':
+            result = x + y;
+            printf("Result: %.f\n", result);
+            break;
+        case '-':
+            result = x - y;
+            printf("Result: %.f\n", result);
+            break;
+        case '*':
+            result = x * y;
+            printf("Result: %.f\n", result);
+            break;
+        case '/':
+            result = x / y;
+            printf("Result: %.2f\n", result);
+            break;
+    }
 }
