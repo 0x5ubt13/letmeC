5d5dd50f2275756582a632416e3943fbf8aed5ea
0x5ubt13 <5ubt13@protonmail.com>
2022-09-04 22:53:07 +0100

    create, free and insert node functions added

--- CS50 labs/speller/dictionary.c
+++ CS50 labs/speller/dictionary.c
@@ -10,24 +10,73 @@
 // Represents a node in a hash table, tweaking to make it a trienode
 typedef struct node
 {
-    char word[LENGTH + 1];
-    struct node *next;
+    char ch;
+    struct node *next[LENGTH + 1];
     bool is_word;
 }
 node;
 
 // Helper function to create a new node, allocating mem and setting all children to null
-node *create_node() {
-    node *new_node = malloc(sizeof(new_node))
+node *create_node() 
+{
+    node *root = malloc(sizeof(node))
 
     for (int i = 0; i < LENGTH; i++)
     {
-        new_node->next[i] = NULL;
+        root->next[i] = NULL;
     }
-    new_node->is_word = false;
+    root->is_word = false;
 
-    return new_node;
+    return root;
 }
+
+// Get it cleaned up recursively
+void free_node(node *to_free) 
+{
+    for (int i = 0; i < LENGTH; i++)
+    {
+        if (to_free->next[i] != NULL)
+        {
+            free_node(to_free->next[i]);
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    free(to_free);
+}
+
+
+// Insert a new word in the table
+node insert_node(node *root, char *word) 
+{
+    // Set temp node to root to start traversing the tree
+    node *temp = root;
+
+    // Loop over every letter of the word
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        // Get the number of the letter 
+        int index = (int)word[i] - 'a';
+        
+        // Create new child if empty
+        if (temp->children[index] == NULL)
+        {
+            temp->children[index] = create_node(word[i]);
+        }
+        
+        // Follow the child
+        temp = temp->children[index];
+    }    
+
+    // End of the work, mark current node as word
+    temp->is_word = true;
+
+    return root;
+}
+
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
