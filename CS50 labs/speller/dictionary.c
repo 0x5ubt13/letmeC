e1248bd5815fc1f4fd29bb65a97524c0eafd1bee
0x5ubt13 <5ubt13@protonmail.com>
2022-09-05 17:23:55 +0100

    implemented check and hash

--- CS50 labs/speller/dictionary.c
+++ CS50 labs/speller/dictionary.c
@@ -7,19 +7,19 @@
 
 #include "dictionary.h"
 
-// Represents a node in a hash table, tweaking to make it a trienode
+// Represents a node in a hash table, tweaking to make it a trie node
 typedef struct node
 {
     char ch;
     struct node *next[LENGTH + 1];
-    bool is_word;
+    bool is_word; 
 }
 node;
 
 // Helper function to create a new node, allocating mem and setting all children to null
 node *create_node() 
 {
-    node *root = malloc(sizeof(node))
+    node *root = malloc(sizeof(node));
 
     for (int i = 0; i < LENGTH; i++)
     {
@@ -48,12 +48,13 @@ void free_node(node *to_free)
     free(to_free);
 }
 
-
-// Insert a new word in the table
-node insert_node(node *root, char *word) 
+// Helper function to insert a new word in the table
+node insert_node(node *root, char *raw_word) 
 {
-    // Set temp node to root to start traversing the tree
+    // Set temp node pointer to root to start traversing the tree
     node *temp = root;
+
+    char *word = tolower(raw_word);
 
     // Loop over every letter of the word
     for (int i = 0; word[i] != '\0'; i++)
@@ -62,77 +63,107 @@ node insert_node(node *root, char *word)
         int index = (int)word[i] - 'a';
         
         // Create new child if empty
-        if (temp->children[index] == NULL)
+        if (temp->next[index] == NULL)
         {
-            temp->children[index] = create_node(word[i]);
+            temp->next[index] = create_node(word[i]);
         }
         
         // Follow the child
-        temp = temp->children[index];
+        temp = temp->next[index];
     }    
 
-    // End of the work, mark current node as word
+    // End of the word, mark current node as word
     temp->is_word = true;
 
     return root;
 }
 
-
-// TODO: Choose number of buckets in hash table
+// Choose number of buckets in hash table
 const unsigned int N = 26;
 
-// Hash table
+// Hash table - modified to be the root of the trie node
 node *table[N];
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    node *temp = table;
+    char *lowercase_word = tolower(word);
+
+    for (int i = 0; lowercase_word[i] != '\0'; i++)
+    {
+        // Get the numerical value for the letter
+        int pos = lowercase_word[i] - 'a'; 
+        
+        // Check whether it exists
+        if (temp->next == NULL)
+        {
+            return false;
+        }
+        
+        // If exists, traverse to next position
+        temp = temp->next[pos];
+    }
+
+    // If traversed to the end of the word and trie node is true, return check successful
+    if (temp != NULL && temp->is_word == true)
+    {
+        return true;
+    }
+    
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    // Return the numerical number of each letter in an array together?
-    return toupper(word[0]) - 'A';
+    // Return the numerical number of each letter plus the incremental value of i to avoid collisions
+    char *lowercase_word = tolower(word);
+
+    int hashed_result = 0;
+
+    for (int i = 0; lowercase_word[i] = '\0'; i++)
+    {
+        hashed_result += lowercase_word[i] - 'A' + i;
+    }
+    
+    return hashed_result;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
-    // Open dictionary file
+    // Open dictionary file, create root node and declare space for words
     FILE *f = fopen(dictionary, "r"); 
     if (f == NULL) 
     { 
         return false; 
     }
 
-    char next_word[LENGTH];
+    table = create_node();
+    char *next_word = malloc(sizeof(char) * LENGTH);
+    if (next_word == NULL)
+    {
+        return false;
+    }
     
     // Read strings from file one at a time
     while (fscanf(f, "%s", next_word) != EOF)
     {
-        // Parse letter by letter
-        for (int i = 0; i < strlen(next_word); i++) 
-        {
-            //not convinced by this, will research other options
-        }
+        // Load word in the trie node
+        table = insert_node(table, next_word);
 
         // Hash word to obtain a hash value
-        int index = hash(new_node);
+        int new_hash = hash(next_word);
 
         // Insert node into hash table at that location
-
+        // skipping for now
     }
 
     fclose(f);
+    
     return true;
-
-    // If unsuccessful, return false
-    return false;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
